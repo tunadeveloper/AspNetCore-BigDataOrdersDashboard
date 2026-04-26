4ad7c99b38f6721d9fdf3a46cacdf10d917ffd23
tunadeveloper <tunabusiness25@gmail.com>
2026-04-26 20:19:42 +0300

    Add project files.

---
+++ AspNetCore-BigDataOrdersDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore_BigDataOrdersDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
