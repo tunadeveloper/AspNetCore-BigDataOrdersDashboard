54b009d90c0d387dfee88c101f37beeda969fa3b
tunadeveloper <tunabusiness25@gmail.com>
2026-04-26 20:27:35 +0300

    Updated

---
+++ AspNetCore-BigDataOrdersDashboard/Controllers/StatisticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_BigDataOrdersDashboard.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
