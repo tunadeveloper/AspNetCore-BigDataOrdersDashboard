d196be5bf6e3b9d2ca8905e934f9683919bd886e
tunadeveloper <tunabusiness25@gmail.com>
2026-04-27 12:57:33 +0300

    Updated

---
+++ AspNetCore-BigDataOrdersDashboard/Context/BigDataOrdersDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AspNetCore_BigDataOrdersDashboard.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore_BigDataOrdersDashboard.Context
+{
+    public class BigDataOrdersDbContext:DbContext
+    {
+        public BigDataOrdersDbContext(DbContextOptions<BigDataOrdersDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
