3e0f251ca528deb279734805e9c282b99b61bb96
tunadeveloper <tunabusiness25@gmail.com>
2026-04-27 12:50:07 +0300

    Updated

---
+++ AspNetCore-BigDataOrdersDashboard/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_BigDataOrdersDashboard.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetCore-BigDataOrdersDashboard/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCore_BigDataOrdersDashboard.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
