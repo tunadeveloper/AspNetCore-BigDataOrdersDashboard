f0aa255c0aa5bef654053962c607b3c516cee571
tunadeveloper <tunabusiness25@gmail.com>
2026-04-27 13:48:56 +0300

    Updated

--- AspNetCore-BigDataOrdersDashboard/Controllers/CategoryController.cs
+++ AspNetCore-BigDataOrdersDashboard/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore_BigDataOrdersDashboard.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCore_BigDataOrdersDashboard.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly BigDataOrdersDbContext _context;
+
+        public CategoryController(BigDataOrdersDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var values = _context.Categories.ToList();
+            return View(values);
         }
     }
 }
